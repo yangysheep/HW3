13e906a5a6081f7bc42145c864acc2ea662c31de
Yan Yang <yy13sz14cz@tamu.edu>
2017-02-27 04:00:30 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,15 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.order(params[:sort_by])
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    end
     @sort_column = params[:sort_by]
+    @all_ratings = Movie.all_ratings
+    @set_ratings = params[:ratings]
+    if !@set_ratings
+      @set_ratings = Hash.new
+    end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        a = Array.new
+        self.select("rating").uniq.each {|x| a.push(x.rating)}
+        a.sort.uniq
+    end
 end
